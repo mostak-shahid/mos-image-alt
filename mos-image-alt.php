f3dea37ef8bf8202b8fa4cd6f587300a9cdb14aa
Md. Mostak Shahid <mostak.shahid@gmail.com>
2018-09-12 01:22:20 +0600

    1.0.0

--- mos-image-alt.php
+++ mos-image-alt.php
@@ -3,7 +3,7 @@
 Plugin Name: Mos Image Alter tag management
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
 Description: A brief description of the Plugin.
-Version: 1.0
+Version: 1.0.0
 Author: Md. Mostak Shahid
 Author URI: http://URI_Of_The_Plugin_Author
 License: GPL2
@@ -16,3 +16,9 @@ Any other notes about the plugin go here
 require_once ( plugin_dir_path( __FILE__ ) . 'mos-image-alt-functions.php' );
 require_once ( plugin_dir_path( __FILE__ ) . 'mos-image-alt-metabox.php' );
 require_once ( plugin_dir_path( __FILE__ ) . 'mos-image-alt-settings.php' );
+require_once('plugins/update/plugin-update-checker.php');
+$pluginInit = Puc_v4_Factory::buildUpdateChecker(
+	'https://raw.githubusercontent.com/mostak-shahid/update/master/mos-image-alt.json',
+	__FILE__,
+	'mos-image-alt'
+);
